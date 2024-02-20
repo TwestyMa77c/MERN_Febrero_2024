73928bf0f4cc38f2e25a7e835ee6a21ce1af8cb8
Matias Cabañas <twestypls@gmail.com>
2024-02-20 11:33:08 -0300

    intentando publicar mi primer react

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -1,22 +1,36 @@
+// src/App.js
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        {/* Contenedor para Matt Estuvo Aquí */}
+        <div className="matt-container">
+          <p className="matt-text">Matt Estuvo Aquí.</p>
+        </div>
+
+        {/* Contenedor para el juego */}
+        <div className="game-container">
+          <iframe
+            title="Ninja Game"
+            src="https://twestyma77c.github.io/Pre-Bootcamp-P-blico/"
+            width="800"
+            height="600"
+            frameBorder="0"
+          ></iframe>
+        </div>
+
+        <p>
           Learn React
-        </a>
+        </p>
       </header>
     </div>
   );
